b8b71b098cfa8aef45b7c80b1fad21b56ef46716
Aghavni Zatikyan <asya.zatikyan.24@gmail.com>
2023-02-04 11:19:45 +0300

    Add Example010_MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {11,12,34,4,15,6,7,8};
+
+int n = array.Length;
+int find = 7;
+
+int index = 0;
+
+while (index<n)
+{
+  if(array[index]== find)
+  {
+   Console.WriteLine(index);
+   break;
+  }
+  index = index + 1;  
+}
