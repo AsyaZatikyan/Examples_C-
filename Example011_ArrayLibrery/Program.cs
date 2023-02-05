41f8c4ba5c73fef22fef1eef2c554f99255b6052
Aghavni Zatikyan <asya.zatikyan.24@gmail.com>
2023-02-05 11:58:47 +0300

    Add Example 011 Array Librery

---
+++ Example011_ArrayLibrery/Program.cs
@@ -0,0 +1,23 @@
+﻿void FillArray(int [] collection);
+{
+int length = collection.Lenghs;
+int index = 0;
+while (index < length)
+    Collection[index] = new Random().Next(1,10);
+    index++;
+}
+
+void PrintArray (int[] col);
+{
+  int count = col.Lenghs;
+  int position = 0;
+  while (position < count)
+  {
+    Console.WriteLine(col.[position]);
+    position ++;
+  }
+}
+int [] array = new int [10];
+FillArray(array);
+PrintArray(array);
+
