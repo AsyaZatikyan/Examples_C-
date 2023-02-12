1396ac1bb4d3498730125b13ce05599e3d47fe5e
Aghavni Zatikyan <asya.zatikyan.24@gmail.com>
2023-02-12 15:48:46 +0300

    Add Example013_Rec ursionAlgorithm

---
+++ Example013_RecursionAlgorithm/Program.cs
@@ -0,0 +1,43 @@
+﻿string[,] table = new string[2, 5];
+// String. Empty
+// table[0,o] table[0,1] table [0,2]
+// table[1,01 table[1,1] table [1,2]
+/* 
+table[1, 2] = "слово";
+
+for (int rows = 0; rows < 2; rows++)
+{
+   for (int  columen = 0; columen < 5; columen++)
+   {
+    Console.WriteLine($"{table [rows,columen]}--");
+   } 
+}*/
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray (int [,] matrix)
+{
+   for (int i = 0; i < matrix.GetLength(0); i++)
+   {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i,j] = new Random().Next(1,10);
+    }
+   } 
+   Console.WriteLine();
+}
+
+int[,] matrix = new int[3, 4];8
+PrintArray(matrix);
+FillArray(matrix);
+PrintArray(matrix);
